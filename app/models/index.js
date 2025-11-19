8a12e279ead4302c26a6c53e20b5de6a075bd580
JIMENENDEZ25 <jmenendeza3@miumg.edu.gt>
2025-11-18 21:56:31 -0600

    ligeros cambios

--- app/models/index.js
+++ app/models/index.js
@@ -1,6 +1,5 @@
 const dbConfig = require("../../config/db.config.js");
 const Sequelize = require("sequelize");
-const { dialect } = require("../config/db.config");
 const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
     host: dbConfig.HOST,
     dialect: dbConfig.dialect,
--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.get("/", (req, res) => {
 
 require("./app/routes/product.route.js")(app);
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8081;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
 });
